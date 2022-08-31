6123b25e8c88f577b0f676627a8cc5f55cd3e1b5
xuwei-k <6b656e6a69@gmail.com>
2022-08-31 15:48:21 +0900

    separate ConnectionSpec. update test redis version

--- install-redis.sh
+++ install-redis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
-REDIS_VERSION="5.0.14"
+REDIS_VERSION="6.2.7"
 wget https://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz
 tar -xzvf redis-${REDIS_VERSION}.tar.gz
 cd redis-${REDIS_VERSION} && make
---
+++ src/test/scala/redis/commands/AuthUsernameAndPasswordSpec.scala
@@ -0,0 +1,47 @@
+package redis.commands
+
+import redis._
+import redis.protocol.Status
+import scala.concurrent.Await
+import java.io.InputStream
+import java.io.OutputStream
+import scala.io.Source
+import scala.sys.process._
+
+class AuthUsernameAndPasswordSpec extends RedisStandaloneServer {
+  sequential
+
+  "AUTH with username and password" should {
+    "ok" in {
+      // TODO When we support ACL command, use ACL to create an user instead of redis-cli
+      val command = s"${RedisServerHelper.redisServerPath}/redis-cli -h ${redis.host} -p ${redis.port}"
+      val username = "testuser"
+      val password = "password0"
+      println(command)
+      Process(command)
+        .run(
+          new ProcessIO(
+            (writeInput: OutputStream) => {
+              //
+              Thread.sleep(2000)
+              println(s"acl setuser ${username} on >${password} allcommands allkeys")
+              writeInput.write(s"acl setuser ${username} on >${password} allcommands allkeys\n".getBytes)
+              writeInput.flush
+              writeInput.write("exit\n".getBytes)
+              writeInput.flush
+            },
+            (processOutput: InputStream) => {
+              Source.fromInputStream(processOutput).getLines().foreach { l => println(l) }
+            },
+            (processError: InputStream) => {
+              Source.fromInputStream(processError).getLines().foreach { l => println(l) }
+            },
+            daemonizeThreads = false
+          )
+        )
+        .exitValue()
+      Thread.sleep(5000)
+      Await.result(redis.auth(username = username, password = password), timeOut).toByteString mustEqual Status.okByteString
+    }
+  }
+}
